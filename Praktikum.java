a5199817d813ad0e31afd15ef55762012a2ffe3b
Denis <badpopoff@yandex.ru>
2022-01-27 21:54:29 +0300

    import HashMap and make HashMap object in public class

--- Praktikum.java
+++ Praktikum.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Praktikum {
     public static void main(String[] args) {
-        
+       HashMap<String, String> list = new HashMap<>();
+    
     }
 
    
